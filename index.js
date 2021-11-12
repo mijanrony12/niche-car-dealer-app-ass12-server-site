4973b4426f5e01d6c23ebad384163f6ab5dd9a97
mijanrony12e <mijaninfo12@gmail.comm>
2021-11-13 01:13:24 +0600

    refinish

--- index.js
+++ index.js
@@ -106,6 +106,14 @@ async function run() {
         const user = req.body;
         const result = await usersCollection.insertOne(user)
         res.json(result)
+      });
+      app.put('/users', async (req, res) => {
+        const user = req.body;
+        const filter = { email: user.email }
+        const options = { upsert: true }
+        const updateDoc = { $set: user }
+        const result = await usersCollection.updateOne(filter, updateDoc, options)
+        res.json(result)
       })
       //update role in user
       app.put("/users/admin", async (req, res) => {
